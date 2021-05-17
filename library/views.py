f8efeca202f0eb5fd0c10698652c9e82e697e0ed
mat105 <mat.105@hotmail.com>
2021-05-17 20:16:34 -0300

    Add in memory cache to tale list

--- library/views.py
+++ library/views.py
@@ -1,3 +1,5 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django_filters import rest_framework as filters
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions
@@ -14,6 +16,10 @@ class TaleViewSet(ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = TaleFilter
+
+    @method_decorator(cache_page(60))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
--- mediatory/settings.py
+++ mediatory/settings.py
@@ -98,6 +98,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'mediatory-base',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
